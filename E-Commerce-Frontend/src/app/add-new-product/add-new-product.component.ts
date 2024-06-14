7404746210f3828f84bb0b5625241952cc8e5ea8
KJCH30 <kinjish30@gmail.com>
2024-06-14 18:58:36 +0530

    Increased image upload size, Added exception handling in the frontend to show error message. Resolved the bug of redirection to login page

--- E-Commerce-Frontend/src/app/add-new-product/add-new-product.component.ts
+++ E-Commerce-Frontend/src/app/add-new-product/add-new-product.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Product } from '../_model/product.model';
 import { NgForm } from '@angular/forms';
 import { ProductService } from '../_services/product.service';
-import { response } from 'express';
-import { error } from 'console';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ImageHandle } from '../_model/image-handle.model';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -11,7 +9,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-add-new-product',
   templateUrl: './add-new-product.component.html',
-  styleUrl: './add-new-product.component.css'
+  styleUrls: ['./add-new-product.component.css']
 })
 export class AddNewProductComponent {
   product: Product = {
@@ -20,63 +18,72 @@ export class AddNewProductComponent {
     productDiscountedPrice: 0.0,
     productActualPrice: 0.0,
     productImages: []
-  }
+  };
+
+  errorMessage: string = '';
 
   constructor(private productService: ProductService,
     private sanitizer: DomSanitizer
   ){}
+
   addProduct(productForm: NgForm){
-    const productFormData = this.prepareFormData(this.product)
+    const productFormData = this.prepareFormData(this.product);
     this.productService.addProduct(productFormData).subscribe({
-      next: (response: Product)=>{
+      next: (response: Product) => {
         console.log(response);
-        productForm.reset()
-        this.product.productImages = []
+        productForm.reset();
+        this.product.productImages = [];
+        this.errorMessage = ''; // Clear the error message on success
       },
-      error: (error: HttpErrorResponse)=>{
+      error: (error: HttpErrorResponse) => {
         console.log(error);
+        if (error.status === 417) { // Expectation Failed status
+          this.errorMessage = 'File too large!';
+        } else {
+          this.errorMessage = 'An error occurred. Please try again.';
+        }
       }
     });
   }
 
-  prepareFormData(product: Product):FormData{
+  prepareFormData(product: Product): FormData {
     const formData = new FormData();
 
     formData.append(
       'product',
        new Blob([JSON.stringify(product)], {type: 'application/json'})
-    )
+    );
 
     for(let i = 0; i < product.productImages.length; i++){
       formData.append(
         'imageFile', 
         product.productImages[i].image_file,
         product.productImages[i].image_file.name
-      )
+      );
     }
 
     return formData;
-
   }
 
-  handleFileInput(event : any){
-    if(event.target.files){
+  handleFileInput(event: any) {
+    if (event.target.files) {
       const file = event.target.files[0];
 
-      const imageHandle : ImageHandle = {
-        image_file : file,
+      const imageHandle: ImageHandle = {
+        image_file: file,
         url: this.sanitizer.bypassSecurityTrustUrl(
           window.URL.createObjectURL(file)
         )
-      }
+      };
       this.product.productImages.push(imageHandle);
     }
   }
-  removeImages(i: number){
-    this.product.productImages.splice(i, 1)
+
+  removeImages(i: number) {
+    this.product.productImages.splice(i, 1);
   }
 
-  fileDropped(imageHandle: ImageHandle){
-    this.product.productImages.push(imageHandle)
+  fileDropped(imageHandle: ImageHandle) {
+    this.product.productImages.push(imageHandle);
   }
 }
