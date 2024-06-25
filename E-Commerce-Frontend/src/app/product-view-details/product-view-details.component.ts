ac92205161a24ec6e65682b914dd1e1c985756b5
KJCH30 <kinjish30@gmail.com>
2024-06-25 23:25:38 +0530

    API for add to cart is injected in product service ts file

--- E-Commerce-Frontend/src/app/product-view-details/product-view-details.component.ts
+++ E-Commerce-Frontend/src/app/product-view-details/product-view-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../_model/product.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../_services/product.service';
 
 @Component({
   selector: 'app-product-view-details',
@@ -12,12 +13,26 @@ export class ProductViewDetailsComponent implements OnInit{
   selectedImage = 0
   product!: Product;
   constructor(private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private productService: ProductService
   ) { }
   ngOnInit(): void {
     this.product = this.activatedRoute.snapshot.data['product']
     console.log(this.product);
     
+  }
+
+  addToCart(productId : number){
+    this.productService.addToCart(productId).subscribe({
+      next: (response) =>{
+        console.log(response);  
+      },
+
+      error: (error) =>{
+        console.log(error);
+      }
+
+    })    
   }
 
   changeImage(index : any){
