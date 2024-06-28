014de8e946846aab645c712f9ab92489bbaf37ab
KJCH30 <kinjish30@gmail.com>
2024-06-28 19:12:31 +0530

    Made changes in the UI

--- E-Commerce-Frontend/src/app/product-view-details/product-view-details.component.ts
+++ E-Commerce-Frontend/src/app/product-view-details/product-view-details.component.ts
@@ -9,39 +9,51 @@ import { CartService } from '../cart.service';
   templateUrl: './product-view-details.component.html',
   styleUrl: './product-view-details.component.css'
 })
-export class ProductViewDetailsComponent implements OnInit{
+export class ProductViewDetailsComponent implements OnInit {
 
   selectedImage = 0
   product!: Product;
+  cartStatus = false;
+
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private productService: ProductService,
-    private cartService: CartService 
+    private cartService: CartService
   ) { }
   ngOnInit(): void {
     this.product = this.activatedRoute.snapshot.data['product']
     console.log(this.product);
-    
+
   }
 
-  addToCart(productId : number){
+  addToCart(productId: number) {
     this.productService.addToCart(productId).subscribe({
-      next: (response) =>{
+      next: (response) => {
         console.log(response);
         if (response) {  // Check if cartId is present in response
           this.cartService.incrementCartCount();
+          this.cartStatus = true;
+          setTimeout(() => {
+            this.cartStatus = false;
+          }, 3000); // Reset the status after 3 seconds
         }
       },
 
-      error: (error) =>{
+      error: (error) => {
         console.log(error);
       }
 
-    })    
+    })
   }
 
-  changeImage(index : any){
-    this.selectedImage = index
+  changeImage(index: number) {
+    if (index >= 0 && index < this.product.productImages.length) {
+      this.selectedImage = index;
+    } else if (index < 0) {
+      this.selectedImage = this.product.productImages.length - 1;
+    } else {
+      this.selectedImage = 0;
+    }
   }
 
   buyProduct(productId: any) {
@@ -52,5 +64,5 @@ export class ProductViewDetailsComponent implements OnInit{
       }
     });
   }
-  
+
 }
